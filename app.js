164786a5aa069491037db1094c16cd8f1c1c547a
Mandi Linville <mandi.linville@gmail.com>
2017-05-01 11:25:44 -0600

    updates

--- app.js
+++ app.js
@@ -6,28 +6,58 @@ function ItunesController() {
         var artist = e.target.artist.value;
         itunesService.getMusicByArtist(artist).then(drawSongs);
     }
-        itunesService.getMusicByArtist('hello').then(drawSongs);
+    itunesService.getMusicByArtist('hello').then(drawSongs);
     function drawSongs(songList) {
         // console.log(songList);
         var template = ""
+        var id = 0
         for (var i = 0; i < songList.length; i++) {
             var song = songList[i]
-            if(song.collection){
-            template +=
-                `<div id = "song-list" class="col-md-4">
+            if (song.collection) {
+                template +=
+                    `
+                    <div id = "song-list" class="col-md-4">
                     <div class="img-container">
                     <img class='album-art' src='${song.albumArt}'>
-                    <h3 class ="title">${song.title.slice(0,24)}</h3>
+                    <h3 class ="title" onclick="itunesCtrl.playController(${id})">${song.title.slice(0, 24)}</h3>
                     <h4>${song.artist}</h4>
-                    <h4>${song.collection.slice(0,30)}</h4>
+                    <h4>${song.collection.slice(0, 30)}</h4>
                     <h4><i class="fa fa-credit-card" aria-hidden="true"></i> $${song.price}</h4>
-                    </h4><audio controls><source src="${song.preview}" type="audio/mpeg"></audio></h4>
+                    </h4><audio class="songControl" controls id="${id}"><source src="${song.preview}" type="audio/mpeg"></audio></h4>   
                     </div>
-                    </div>`
-        }}
+                    </div>
+                    `
+                id++
+            }
+        }
         document.getElementById('songs').innerHTML = template
     }
+    var currentSongPlaying;
+    var isPlaying = false
+
+    this.playController = function (id) {
+        var start = document.getElementById(id)
+        if (currentSongPlaying && isPlaying) {
+            currentSongPlaying.pause();
+            isPlaying = false
+        }
+        currentSongPlaying = start
+        isPlaying = true;
+        start.play();
+    }
+
+    document.addEventListener('play', function (e) {
+    var audios = document.getElementsByTagName('audio');
+    for (var i = 0, len = audios.length; i < len; i++) {
+      if (audios[i] != e.target) {
+        audios[i].pause();
+      }
+    }
+  }, true);
+
 }
+
+
 
 
 
